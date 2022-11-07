d29dcc701f113682101294d4dd42b48d8c007c8e
MammothsCC <117637961+MammothsCC@users.noreply.github.com>
2022-11-07 10:50:23 +0100

    Update FlexibleTranslationMethod.cs

--- FlexibleTranslationMethod.cs
+++ FlexibleTranslationMethod.cs
@@ -1,52 +1,33 @@
-﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Franky
 {
     class FlexibleTranslationMethod
     {
-        
-        static void Main(string[] args)
+
+        string TranslationMethod(Dictionary<string, string> translation, string value)
         {
-            #region Bevor die Positionen beginnen erstellen der Translation. Damit wir sie nicht für jede Position angelegt.
+            value = $"|{value}|";
+            bool translationFound = false;
+            foreach (string key in translation.Keys)
+            {
+                string values = translation[key];
+                values = $"|{values.Replace(',', '|')}|";
 
-            Dictionary<string, string> trans = new Dictionary<string, string>();
-            trans.Add("Hallo", "Hello,Salute,Tschau");
-            trans.Add("MM", "Millimeter,Kleinermeta");
-            trans.Add("M", "Meter,Großermeter");
-            
-            #endregion
-
-            //An dem Punkt verwenden wo die Translation beginngen soll und statt Liter den eingelesen Wert verwenden welcher Übersetzt werden soll.
-            string x = FlexibleTranslationMethod(trans, "Meter");
-
-
-            string FlexibleTranslationMethod(Dictionary<string, string> translation, string value)
-            {                
-                value = $"|{value}|";
-                bool translationFound = false;
-                foreach(string key in translation.Keys)
+                if (values.Contains(value))
                 {
-                    string values = translation[key];
-                    values = $"|{values.Replace(',','|')}|";
-
-                    if (values.Contains(value))
-                    {
-                        value = key;
-                        translationFound = true;
-                        break;
-                    }
-
+                    value = key;
+                    translationFound = true;
+                    break;
                 }
-                if (!translationFound)
-                {
-                    //internalexception
-                }
-                return value;
+
             }
+            if (!translationFound)
+            {
+                //internalexception
+            }
+            return value;
+
 
         }
     }
